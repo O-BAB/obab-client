3d8b4d9da0e47591c785a43e39311df87122ec66
BerkleyLim <berkleylim16@gmail.com>
2024-03-13 21:19:55 +0900

    feat: create Login component - 로그인 컴포넌트 추가 - css 추가 - 아직 구현하지 않는 상태, 클린코드 고민 중 - 폴더명과 파일명 규칙을 어떻게 할지 고민중

---
+++ src/component/login_user/LoginUser.jsx
@@ -0,0 +1,12 @@
+import styles from "../login_user/login_user.module.css";
+import React from "react";
+
+const LoginUser = (props) => {
+    return (
+        <div>
+            <div>로그인</div>
+        </div>
+    )
+}
+
+export default LoginUser
