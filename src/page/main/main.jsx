4795cd0d1e2016932cf8902358cb0635349a7f92
BerkleyLim <berkleylim16@gmail.com>
2024-03-14 23:57:15 +0900

    style: set formatter on main.jsx - 서식 지정

--- src/page/main/main.jsx
+++ src/page/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Navbar from 'component/navbar/navbar';
 import SiteInfo from 'component/siteinfo/siteinfo';
 import TodayPick from 'component/main_today_pick/main_today_pick';
@@ -20,31 +20,31 @@ const Main = () => {
     setSelectedCategory(category);
   };
 
-    /**
-     * 이 부분은 개발이 끝나면 제거 할 것!
-     * console.log는 가급적이면 사용자가 보이지 않게 한다.
-     */
+  /**
+   * 이 부분은 개발이 끝나면 제거 할 것!
+   * console.log는 가급적이면 사용자가 보이지 않게 한다.
+   */
   // console.log('Selected Category:', selectedCategory); // 이 부분을 추가하여 selectedCategory 값을 콘솔에 출력합니다.
 
   return (
     <>
-      <Navbar onCategorySelect={handleCategorySelect} />
+      <Navbar onCategorySelect={handleCategorySelect}/>
       {selectedCategory === null ? (
         <>
-          <SiteInfo />
-          <TodayPick />
-          <SoupUpdate />
-          <CookingTip />
-          <MostRead />
-          <Cooking />
-          <Recipe />
-          <ConvenienceRecipe />
-          <TopButton />
+          <SiteInfo/>
+          <TodayPick/>
+          <SoupUpdate/>
+          <CookingTip/>
+          <MostRead/>
+          <Cooking/>
+          <Recipe/>
+          <ConvenienceRecipe/>
+          <TopButton/>
         </>
       ) : (
-        <CategoryList category_title={selectedCategory} />
+        <CategoryList category_title={selectedCategory}/>
       )}
-      <Footer />
+      <Footer/>
     </>
   );
 };
