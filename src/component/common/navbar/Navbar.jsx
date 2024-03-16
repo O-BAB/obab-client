561481e428b91e06b9b8796b06c9d109427b61cf
BerkleyLim <berkleylim16@gmail.com>
2024-03-16 22:54:38 +0900

    feat: change ui on header - header 일부 수정 및 추가 - 마이페이지 기능 추가

--- src/component/common/navbar/Navbar.jsx
+++ src/component/common/navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import styles from '../../../css/common/navbar/navbar.module.css';
 import Logo from "./item/Logo";
 import CATEGORIES from "./data/CATEGORIES";
 import MenuBarList from "./item/MenuBarList";
-import commonContainer from "../../../hooks/commonContainer";
 
 function Navbar() {
 // function Navbar({ onCategorySelect }) {
@@ -11,33 +10,37 @@ function Navbar() {
 //     const handleCategoryClick = (category) => {
 //         onCategorySelect(category.name);
 //     };
-    /**
-     * 위에 함수는 Origin Component 용으로 사용 된 것으로, 굳이 중복되는 부분을 사용할 필요 없다.
-     * 자세한 사항은 React Design Pattern 을 검색하고 학습 습득하는 것을 확인 하면 좋다.
-     * 이 중 Custom Hook Pattern 으로 구조를 나눠 사용한다.
-     */
-    const {handleCategorySelect} = commonContainer()
+  /**
+   * 위에 함수는 Origin Component 용으로 사용 된 것으로, 굳이 중복되는 부분을 사용할 필요 없다.
+   * 자세한 사항은 React Design Pattern 을 검색하고 학습 습득하는 것을 확인 하면 좋다.
+   * 이 중 Custom Hook Pattern 으로 구조를 나눠 사용한다.
+   */
+  // const {handleCategorySelect} = commonContainer()
 
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.navelement}>
-                <div className={styles.navcontainer}>
-                    <Logo />
-                    <div className={styles.categorycontainer}>
-                        <ul className={styles.menu}>
-                            {/* key={index} 넣어줘야 console에 빨간색 에러 방지 */}
-                            {CATEGORIES.map((category, index) => (
-                                <MenuBarList key={index} category={category} />
-                            ))}
-                        </ul>
-                    </div>
-                    <div className={`cursor-pointer hover:text-blue-600`}>
-                        로그인 버튼
-                    </div>
-                </div>
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navelement}>
+        <div className={styles.navcontainer}>
+          <Logo/>
+          <ul className={styles.menu}>
+            {/* key={index} 넣어줘야 console에 빨간색 에러 방지 */}
+            {CATEGORIES.map((category, index) => (
+              <MenuBarList key={index} category={category}/>
+            ))}
+          </ul>
+          {/*<div className={`flex flex-row flex-wrap justify-end`}>*/}
+          <div className={`${styles.navbarRight} px-5`}>
+            <div className={`${styles.navbarRightItem} cursor-pointer hover:text-blue-700 `}>
+              마이페이지
             </div>
-        </nav>
-    );
+            <div className={`${styles.navbarRightItem} cursor-pointer hover:text-blue-700 `}>
+              로그인 버튼
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
