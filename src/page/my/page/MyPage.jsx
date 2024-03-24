90c7001f0d4af4a878aa9d35c165256cbb20169e
BerkleyLim <berkleylim16@gmail.com>
2024-03-24 20:54:40 +0900

    feat: design UI component on mypage - 왼쪽 컴포넌트 메뉴바 추가 - 현재, 반응형 테스트 진행 해야함 (프로필 부분 수정 필요)

--- src/page/my/page/MyPage.jsx
+++ src/page/my/page/MyPage.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import Navbar from "../../../component/common/navbar/Navbar";
 import styles from '../../../css/my/page/myPage.module.css'
+import {PersonCircle} from "react-bootstrap-icons";
 
 const MyPage = () => {
   return (
     <div>
       <Navbar />
       <div className={`${styles?.myPageComponent}`}>
-        <div className={`${styles?.myPageLeftComponent} py-5 px-4`}>
-          left : 메뉴를 고정으로 박는다.
-          <div className={``}>
-           본인 이미지
+        <div className={`${styles?.myPageLeftComponent} py-5 px-7`}>
+          <div className={`${styles?.myPageLeftProfileImageComponent}`}>
+            <PersonCircle className={`${styles?.myPageLeftProfileImage}`}/>
           </div>
-          <div className={``}>
-            닉네임
+          <div className={`font-bold text-xl my-4`}>
+            {`닉네임`}
           </div>
-          <div className={``}>
-            간략 자기소개
+          <div className={`justify-items-start text-left px-7 my-5`}>
+            {`간단한 자기소개 두줄 정도 가나다라마바사 아자차`}
           </div>
-          <div className={``}>
-            페이스북
-            트위터
-            유튜브 등록
+          <div className={`justify-items-start text-left px-7 my-5`}>
+            <div className={`flex-col cursor-pointer my-3 `}>
+              {`페이스북`}
+            </div>
+            <div className={`flex-col cursor-pointer my-3`}>
+              {`트위터`}
+            </div>
+            <div className={`flex-col cursor-pointer my-3`}>
+              {`유튜브`}
+            </div>
           </div>
         </div>
         <div className={`${styles?.myPageRightComponent} py-5 px-4`}>
