046872cbc4b89446178d4a0c677b53b57cb2c7b1
BerkleyLim <berkleylim16@gmail.com>
2024-03-24 17:46:20 +0900

    feat: design UI component on mypage - 마이페이지 레이아웃 설계 - 영역을 표시하기 위해 임시로 색깔을 표시함 - 영역 만들면 컴포넌트 단위로 나눠서 구조를 나눌 예정

--- src/page/my/page/MyPage.jsx
+++ src/page/my/page/MyPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Navbar from "../../../component/common/navbar/Navbar";
+import styles from '../../../css/my/page/myPage.module.css'
 
 const MyPage = () => {
   return (
     <div>
       <Navbar />
+      <div className={`${styles?.myPageComponent}`}>
+        <div className={`${styles?.myPageLeftComponent}`}>
+          left
+        </div>
+        <div className={`${styles?.myPageRightComponent}`}>
+          right
+        </div>
+      </div>
     </div>
   );
 }
