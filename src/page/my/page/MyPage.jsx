2b5a54d15bf1c6f0f5819fca16ad4889f76891f1
BerkleyLim <berkleylim16@gmail.com>
2024-03-24 17:51:35 +0900

    feat: design UI component on mypage - 마이페이지 레이아웃 설계 - 영역을 표시하기 위해 임시로 색깔을 표시함 - 구성 요소만 대충 작성

--- src/page/my/page/MyPage.jsx
+++ src/page/my/page/MyPage.jsx
@@ -7,11 +7,26 @@ const MyPage = () => {
     <div>
       <Navbar />
       <div className={`${styles?.myPageComponent}`}>
-        <div className={`${styles?.myPageLeftComponent}`}>
-          left
+        <div className={`${styles?.myPageLeftComponent} py-5 px-4`}>
+          left : 메뉴를 고정으로 박는다.
+          <div className={``}>
+           본인 이미지
+          </div>
+          <div className={``}>
+            닉네임
+          </div>
+          <div className={``}>
+            간략 자기소개
+          </div>
+          <div className={``}>
+            페이스북
+            트위터
+            유튜브 등록
+          </div>
         </div>
-        <div className={`${styles?.myPageRightComponent}`}>
-          right
+        <div className={`${styles?.myPageRightComponent} py-5 px-4`}>
+          right : 라우터 컴포넌트 영역
+        {/*  이부분은 라우터 컴포넌트를 이용하여 제작하는 영역 */}
         </div>
       </div>
     </div>
