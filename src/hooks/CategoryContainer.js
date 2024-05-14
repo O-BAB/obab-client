cedf92988402e4bc4b2dec20c803b5faf5b14483
BerkleyLim <berkleylim16@gmail.com>
2024-05-14 19:03:14 +0900

    feat: add API GET /recipes/food-recipes
    - API 등록
    - 일부 데이터 형식 확인 필요
    
    obab-client #2

--- src/hooks/CategoryContainer.js
+++ src/hooks/CategoryContainer.js
@@ -7,34 +7,33 @@ const CategoryContainer = () => {
    * @returns {{displayCategoryTitle: ((function(): string)|*)}} : 제목명 반환
    */
   const displayCategoryTitle = (pathname) => {
-    if (pathname === '/korean/cuisine') {
+    if (pathname.includes('/korean/cuisine')) {
       return "Korean Cuisine"
-    } else if (pathname === '/food/recipe') {
+    } else if (pathname.includes('/food/recipe')) {
       return "Food recipe"
-    } else if (pathname === '/cooking/tips') {
+    } else if (pathname.includes('/cooking/tips')) {
       return "Cooking Tips"
-    } else if (pathname === '/recipe') {
-      return "Recipe"
-    } else if (pathname === '/soup/recipe') {
+    } else if (pathname.includes('/soup/recipe')) {
       return "Soup Recipe"
+    } else if (pathname.includes('/recipe')) {
+      return "Recipe"
     } else {
       return "";
     }
   }
 
   // 레시피 목록 출력
-  const displayReceiptList = (page, pageSize, categoryCD) => {
-    console.log(page, pageSize, categoryCD)
+  const displayReceiptList = async (page, pageSize, categoryCD) => {
     if (categoryCD === 'Korean Cuisine') {
-      return connectReceiptList(page, pageSize, 'korean_cuisine')
+      return await connectReceiptList(page, pageSize, 'korean_cuisine')
     } else if (categoryCD === 'Food recipe') {
-      return connectReceiptList(page, pageSize, 'food_recipe')
+      return await connectReceiptList(page, pageSize, 'food_recipe')
     } else if (categoryCD === 'Cooking Tips') {
-      return connectReceiptList(page, pageSize, 'cooking_tip')
+      return await connectReceiptList(page, pageSize, 'cooking_tip')
     } else if (categoryCD === 'Recipe') {
-      return connectReceiptList(page, pageSize, 'recipe')
+      return await connectReceiptList(page, pageSize, 'recipe')
     } else if (categoryCD === 'Soup Recipe') {
-      return connectReceiptList(page, pageSize, 'soup_recipe')
+      return await connectReceiptList(page, pageSize, 'soup_recipe')
     } else {
       return "";
     }
--- src/service/CategoryService.js
+++ src/service/CategoryService.js
@@ -2,11 +2,12 @@ import UseApi from "../util/UseApi";
 
 const CategoryService = () => {
   const connectReceiptList = async (page, pageSize, categoryCD) => {
-    return await UseApi.get(`${process.env.REACT_APP_API_ROOT}recipes/food-recipes?page=${page}&pageSize=${pageSize}&categoryCD=${categoryCD}`)
+    return await UseApi.get(`${process.env.REACT_APP_API_ROOT}recipes/food-recipes/?page=${page}&pageSize=${pageSize}&categoryCD=${categoryCD}`)
       .then((res) => {
-        return res.data;
+        return res.data.data;
       })
       .catch((e) => {
+        console.error(e);
         return e;
       });
   }
