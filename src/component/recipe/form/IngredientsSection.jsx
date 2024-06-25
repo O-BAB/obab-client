15f3445ea83d3f4e1f7eecb9954a1358a0662592
BerkleyLim <berkleylim16@gmail.com>
2024-06-25 16:00:06 +0900

    fix: change Form in recipes
    - 재료, 양념, 삭제 아이콘 부분 한줄로 조정
    
    obab-client #8

--- src/component/recipe/form/IngredientsSection.jsx
+++ src/component/recipe/form/IngredientsSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {useRecoilState} from "recoil";
-import {ingredientsFormState} from "../../../recoil/recipeState";
-import {Button, IconButton, TextField, Typography} from "@mui/material";
-import {Delete} from "@mui/icons-material";
+import { useRecoilState } from "recoil";
+import { ingredientsFormState } from "../../../recoil/recipeState";
+import { Button, IconButton, TextField, Typography, Grid } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
 const IngredientsSection = () => {
-  const [ingredientsForm, setIngredientsForm] = useRecoilState(ingredientsFormState)
+  const [ingredientsForm, setIngredientsForm] = useRecoilState(ingredientsFormState);
+
   const handleIngredientChange = (index, field, value) => {
     const newIngredients = [...ingredientsForm];
     newIngredients[index][field] = value;
@@ -20,43 +21,54 @@ const IngredientsSection = () => {
     const newIngredients = ingredientsForm.filter((_, i) => i !== index);
     setIngredientsForm(newIngredients);
   };
+
   return (
     <div className="mt-8">
       <Typography variant="h6" className="mb-2">재료</Typography>
       {ingredientsForm.map((ingredient, index) => (
-        <div key={index} className="grid grid-cols-5 gap-4 mb-2">
-          <TextField
-            className="col-span-1"
-            variant="outlined"
-            placeholder="재료 명"
-            value={ingredient.name}
-            onChange={(e) => handleIngredientChange(index, 'name', e.target.value)}
-          />
-          <TextField
-            className="col-span-1"
-            variant="outlined"
-            placeholder="수량"
-            value={ingredient.quantity}
-            onChange={(e) => handleIngredientChange(index, 'quantity', e.target.value)}
-          />
-          <TextField
-            className="col-span-1"
-            variant="outlined"
-            placeholder="단위"
-            value={ingredient.unit}
-            onChange={(e) => handleIngredientChange(index, 'unit', e.target.value)}
-          />
-          <TextField
-            className="col-span-2"
-            variant="outlined"
-            placeholder="비고"
-            value={ingredient.notes}
-            onChange={(e) => handleIngredientChange(index, 'notes', e.target.value)}
-          />
-          <IconButton color="secondary" onClick={() => handleRemoveIngredient(index)}>
-            <Delete/>
-          </IconButton>
-        </div>
+        <Grid container spacing={2} alignItems="center" key={index} className="mb-2">
+          <Grid item xs={2}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="재료 명"
+              value={ingredient.name}
+              onChange={(e) => handleIngredientChange(index, 'name', e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="수량"
+              value={ingredient.quantity}
+              onChange={(e) => handleIngredientChange(index, 'quantity', e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="단위"
+              value={ingredient.unit}
+              onChange={(e) => handleIngredientChange(index, 'unit', e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="비고"
+              value={ingredient.notes}
+              onChange={(e) => handleIngredientChange(index, 'notes', e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton color="secondary" onClick={() => handleRemoveIngredient(index)}>
+              <Delete />
+            </IconButton>
+          </Grid>
+        </Grid>
       ))}
       <Button
         color="primary"
